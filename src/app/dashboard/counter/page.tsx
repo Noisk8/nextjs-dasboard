86ffac083053826602dd1b35790948c932f4a16c
noisk8 <juansk8jaramillo@gmail.com>
2024-01-10 00:40:47 -0500

    contador funcional

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,26 @@
 
+
+import { CartCounter } from "@/app/shopping-cart";
+
+
+
+export const metadata = {
+  title: ' contador',
+  description:'contador en nextjs',
+};
+
+
 export default function CounterPage() {
+
+
+
   return (
-    <div>
-      <h1>Counter page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+     <span> Carrito de shop </span>
+     <CartCounter  value={ 7 } />
+    
     </div>
+
+
   );
 }
