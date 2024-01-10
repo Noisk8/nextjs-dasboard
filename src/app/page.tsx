e9c49b8793d159620e268fe4fefe88a4ea898184
noisk8 <juansk8jaramillo@gmail.com>
2024-01-09 23:12:42 -0500

    pegueland

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+
+export default function Home() {
+
+redirect ('/dashboard/counter');
+
+  return (
+    <h1 className='text-withe'>Hola</h1>
+    
+  )
+}
