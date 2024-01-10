2c7cdff6f85db9bd203eb3adbee2e3172b3bd04f
noisk8 <juansk8jaramillo@gmail.com>
2024-01-10 00:51:40 -0500

    skate logo

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 
-import {IoBrowsersOutline, IoCalculator, IoLogoReact} from 'react-icons/io5'
+import {IoBrowsersOutline, IoCalculator, IoLogoReact,  } from 'react-icons/io5'
+import { GiSkateboard } from "react-icons/gi";
 import { SidebarMenuItem } from './SidebarMenuItem'
 
 
@@ -32,12 +33,12 @@ export const Sidebar = () => {
     <div id="logo" className="my-4 px-6">
      <h1 className=" flex items-center text-lg md:text-2xl font-bold text-white">
 
-        <IoLogoReact  className="mr-2"/>
-        Dash<span className="text-blue-500">8</span>.</h1>
-     <p className="text-slate-500 text-sm">Manage your actions and activities</p>
+     <GiSkateboard size={'30px'}  className="mr-2 pb-1"/>
+        Dash<span className="text-blue-500">Sk8</span> Board</h1>
+     <p className="text-slate-500 text-sm text-center">Un dashboard de un patineto</p>
     </div>
     <div id="profile" className="px-6 py-10">
-     <p className="text-slate-500">Welcome back,</p>
+     
      <a href="#" className="inline-flex space-x-2 items-center">
          <span>
              <Image className="rounded-full w-8 h-8" src="/uno.jpeg" alt="foto"  width={500} height={400}/>
