ebb170db73ba21ca794ae32951f099e41438ae8a
noisk8 <juansk8jaramillo@gmail.com>
2024-01-17 03:07:23 -0500

    not-found-granular

--- src/app/dashboard/pokemon/[id]/not-found.tsx
+++ src/app/dashboard/pokemon/[id]/not-found.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className='flex items-center justify-center pt-16'>
-<h1 className="text-9xl font-extrabold text-white tracking-widest">404</h1>
+<h1 className="text-9xl font-extrabold text-white tracking-widest">No existe el pokemon</h1>
 	<div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
 		Page Not Found
 	</div>
--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,63 +1,45 @@
-
 import { Pokemon } from "@/pokemons";
 import { Metadata } from "next";
 
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
-interface Props{
-  params:{
-    id: string
+interface Props {
+  params: {
+    id: string;
   };
 }
 
-export  async function generateMetadata ({ params }: Props): Promise<Metadata>{
-
-
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
   try {
+    const { id, name } = await getPokemon(params.id);
 
-    const {id, name } = await getPokemon(params.id);
-
-
-    return{
-      title:`${id} - Pokedex`,
-      description:`Pagina de ${name} `,
-    }
-  } catch(error){
-return{
-
-  title:'esto que monda',
-  description:'esto que voleta'
-}
-
+    return {
+      title: `${id} - Pokedex`,
+      description: `Pagina de ${name} `,
+    };
+  } catch (error) {
+    return {
+      title: "esto que monda",
+      description: "esto que voleta",
+    };
   }
-
 }
 
+const getPokemon = async (id: string): Promise<Pokemon> => {
+  try {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`, {
+      cache: "force-cache",
+    }).then((resp) => resp.json());
 
-
-const getPokemon = async (id: string): Promise <Pokemon> => {
-  try{
-
-    const pokemon = await fetch (`https://pokeapi.co/api/v2/pokemon/${ id }`,{
-      cache: 'force-cache'
-    }).then (resp => resp.json());
-  
     return pokemon;
-  }
-  
-  catch(error){
+  } catch (error) {
     notFound();
-  
   }
-  
+};
 
- 
-}
-
-
-export default async function PokemonPage({params}: Props){
-  const pokemon =await getPokemon(params.id)
+export default async function PokemonPage({ params }: Props) {
+  const pokemon = await getPokemon(params.id);
 
   return (
     <div className="flex mt-5 flex-col items-center text-slate-800">
@@ -68,49 +50,44 @@ export default async function PokemonPage({params}: Props){
           </h1>
           <div className="flex flex-col justify-center items-center">
             <Image
-              src={pokemon.sprites.other?.dream_world.front_default ?? ''}
+              src={pokemon.sprites.other?.dream_world.front_default ?? ""}
               width={150}
               height={150}
               alt={`Imagen del pokemon ${pokemon.name}`}
               className="mb-5"
             />
 
-
             <div className="flex flex-wrap">
-              {
-                pokemon.moves.map(move => (
-                  <p key={move.move.name} className="mr-2 capitalize">{move.move.name}</p>
-                ))
-              }
+              {pokemon.moves.map((move) => (
+                <p key={move.move.name} className="mr-2 capitalize">
+                  {move.move.name}
+                </p>
+              ))}
             </div>
           </div>
         </div>
         <div className="grid grid-cols-2 gap-4 px-2 w-full">
-
           <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
             <p className="text-sm text-gray-600">Types</p>
             <div className="text-base font-medium text-navy-700 flex">
-              {
-                pokemon.types.map(type => (
-                  <p key={type.slot} className="mr-2 capitalize">{type.type.name}</p>
-                ))
-              }
+              {pokemon.types.map((type) => (
+                <p key={type.slot} className="mr-2 capitalize">
+                  {type.type.name}
+                </p>
+              ))}
             </div>
           </div>
 
           <div className="flex flex-col items-start justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg ">
             <p className="text-sm text-gray-600">Peso</p>
             <span className="text-base font-medium text-navy-700 flex">
-              {
-                pokemon.weight
-              }
+              {pokemon.weight}
             </span>
           </div>
 
           <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
             <p className="text-sm text-gray-600">Regular Sprites</p>
             <div className="flex justify-center">
-
               <Image
                 src={pokemon.sprites.front_default}
                 width={100}
@@ -123,16 +100,13 @@ export default async function PokemonPage({params}: Props){
                 width={100}
                 height={100}
                 alt={`sprite ${pokemon.name}`}
-                
               />
-
             </div>
           </div>
 
           <div className="flex flex-col justify-center rounded-2xl bg-white bg-clip-border px-3 py-4  drop-shadow-lg">
             <p className="text-sm text-gray-600">Shiny Sprites</p>
             <div className="flex justify-center">
-
               <Image
                 src={pokemon.sprites.front_shiny}
                 width={100}
@@ -146,14 +120,10 @@ export default async function PokemonPage({params}: Props){
                 height={100}
                 alt={`sprite ${pokemon.name}`}
               />
-
             </div>
           </div>
-
-
-
         </div>
       </div>
     </div>
-  );}
-  
+  );
+}
